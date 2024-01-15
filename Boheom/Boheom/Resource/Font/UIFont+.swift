24f5703a82aed1a78e1817575e3a4a7c3cfb23cc
Byung jin <bjcho1503@naver.com>
2024-01-15 16:55:07 +0900

    🎨 :: Font Regular 추가

--- Boheom/Boheom/Resource/Font/UIFont+.swift
+++ Boheom/Boheom/Resource/Font/UIFont+.swift
@@ -1,11 +1,13 @@
 import UIKit
 
 public extension UIFont {
+
     // MARK: - Header1
     static let headerH1ExtraBold = UIFont(name: "Pretendard-ExtraBold", size: 30)
     static let headerH1Bold = UIFont(name: "Pretendard-Bold", size: 30)
     static let headerH1SemiBold = UIFont(name: "Pretendard-SemiBold", size: 30)
     static let headerH1Medium = UIFont(name: "Pretendard-Medium", size: 30)
+    static let headerH1Regular = UIFont(name: "Pretendard-Regular", size: 30)
     static let headerH1Light = UIFont(name: "Pretendard-Light", size: 30)
     static let headerH1Thin = UIFont(name: "Pretendard-Thin", size: 30)
 
@@ -14,6 +16,7 @@ public extension UIFont {
     static let headerH2Bold = UIFont(name: "Pretendard-Bold", size: 24)
     static let headerH2SemiBold = UIFont(name: "Pretendard-SemiBold", size: 24)
     static let headerH2Medium = UIFont(name: "Pretendard-Medium", size: 24)
+    static let headerH2Regular = UIFont(name: "Pretendard-Regular", size: 24)
     static let headerH2Light = UIFont(name: "Pretendard-Light", size: 24)
     static let headerH2Thin = UIFont(name: "Pretendard-Thin", size: 24)
 
@@ -22,6 +25,7 @@ public extension UIFont {
     static let headerH3Bold = UIFont(name: "Pretendard-Bold", size: 20)
     static let headerH3SemiBold = UIFont(name: "Pretendard-SemiBold", size: 20)
     static let headerH3Medium = UIFont(name: "Pretendard-Medium", size: 20)
+    static let headerH3Regular = UIFont(name: "Pretendard-Regular", size: 20)
     static let headerH3Light = UIFont(name: "Pretendard-Light", size: 20)
     static let headerH3Thin = UIFont(name: "Pretendard-Thin", size: 20)
 
@@ -30,6 +34,7 @@ public extension UIFont {
     static let bodyB1Bold = UIFont(name: "Pretendard-Bold", size: 16)
     static let bodyB1SemiBold = UIFont(name: "Pretendard-SemiBold", size: 16)
     static let bodyB1Medium = UIFont(name: "Pretendard-Medium", size: 16)
+    static let bodyB1Regular = UIFont(name: "Pretendard-Regular", size: 16)
     static let bodyB1Light = UIFont(name: "Pretendard-Light", size: 16)
     static let bodyB1Thin = UIFont(name: "Pretendard-Thin", size: 16)
 
@@ -38,6 +43,7 @@ public extension UIFont {
     static let bodyB2Bold = UIFont(name: "Pretendard-Bold", size: 14)
     static let bodyB2SemiBold = UIFont(name: "Pretendard-SemiBold", size: 14)
     static let bodyB2Medium = UIFont(name: "Pretendard-Medium", size: 14)
+    static let bodyB2Regular = UIFont(name: "Pretendard-Regular", size: 14)
     static let bodyB2Light = UIFont(name: "Pretendard-Light", size: 14)
     static let bodyB2Thin = UIFont(name: "Pretendard-Thin", size: 14)
 
@@ -46,6 +52,7 @@ public extension UIFont {
     static let bodyB3Bold = UIFont(name: "Pretendard-Bold", size: 13)
     static let bodyB3SemiBold = UIFont(name: "Pretendard-SemiBold", size: 13)
     static let bodyB3Medium = UIFont(name: "Pretendard-Medium", size: 13)
+    static let bodyB3Regular = UIFont(name: "Pretendard-Regular", size: 13)
     static let bodyB3Light = UIFont(name: "Pretendard-Light", size: 13)
     static let bodyB3Thin = UIFont(name: "Pretendard-Thin", size: 13)
 
@@ -54,6 +61,7 @@ public extension UIFont {
     static let captionC1Bold = UIFont(name: "Pretendard-Bold", size: 12)
     static let captionC1SemiBold = UIFont(name: "Pretendard-SemiBold", size: 12)
     static let captionC1Medium = UIFont(name: "Pretendard-Medium", size: 12)
+    static let captionC1Regular = UIFont(name: "Pretendard-Regular", size: 12)
     static let captionC1Light = UIFont(name: "Pretendard-Light", size: 12)
     static let captionC1Thin = UIFont(name: "Pretendard-Thin", size: 12)
 }
